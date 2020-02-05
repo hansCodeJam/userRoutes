ea4516a75a1f5b7bc64f594cf04eebd2f4b73010
Hans Vos <hans.vos@codeimmersives.com>
2020-02-05 17:28:18 -0500

    done

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const path = require('path');
 const uuid = require('uuid/v4');
 const users = require('./models/Users')
 const port = process.env.PORT || '3000';
-
 const userRoutes = require('./routes/userRoutes');
+const singleUserRoutes = require('./routes/singleUserRoutes')
+const postUser = require('./routes/postUser');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', userRoutes)
+app.use('/api/users/:id', singleUserRoutes);
+app.use('/', postUser);
+
 //const {logMe} = require('./middleware/logger')
 //const {myMoment} = require('./middleware/moment')
 
@@ -28,29 +33,29 @@ app.get('/', (req, res) => {
 // })
 
 //get a single user
-app.get('/api/users/:id', (req, res) => {
-    const userExists = users.filter(user => user.id === parseInt(req.params.id));
-    console.log(userExists);
-    if(userExists.length !== 0) {
-        return res.status(200).json(userExists[0]);
-    } else {
-        return res.status(400).json({message: `User with id: ${req.params.id} does not exist` })
-    }
-});
+// app.get('/api/users/:id', (req, res) => {
+//     const userExists = users.filter(user => user.id === parseInt(req.params.id));
+//     console.log(userExists);
+//     if(userExists.length !== 0) {
+//         return res.status(200).json(userExists[0]);
+//     } else {
+//         return res.status(400).json({message: `User with id: ${req.params.id} does not exist` })
+//     }
+// });
 
 // create user
-app.post('/', (req, res) => {
-    if(!req.body.name || !req.body.email) {
-        return res.status(400).json( {message: 'Please enter both a name and an email'})
-    }
-    const newUser = {};
-    newUser.id = uuid();
-    newUser.name = req.body.name;
-    newUser.email = req.body.email;
-    users.push(newUser);
-    console.log(newUser)
-    return res.json(req.body);
-})
+// app.post('/', (req, res) => {
+//     if(!req.body.name || !req.body.email) {
+//         return res.status(400).json( {message: 'Please enter both a name and an email'})
+//     }
+//     const newUser = {};
+//     newUser.id = uuid();
+//     newUser.name = req.body.name;
+//     newUser.email = req.body.email;
+//     users.push(newUser);
+//     console.log(newUser)
+//     return res.json(req.body);
+// })
 
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`)
